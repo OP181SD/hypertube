c6d90a687b0f48f756fc1a7237f486a25afbd6bc
jecointr <104733115+jecointr@users.noreply.github.com>
2026-06-03 23:34:42 +0200

    chore(auth): remove unused passport-local strategy and guard

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -4,7 +4,6 @@ import { JwtModule } from "@nestjs/jwt";
 import { ConfigModule, ConfigService } from "@nestjs/config";
 import { AuthService } from "./auth.service";
 import { AuthController } from "./auth.controller";
-import { LocalStrategy } from "./strategies/local.strategy";
 import { JwtStrategy } from "./strategies/jwt.strategy";
 import { FtStrategy } from "./strategies/ft.strategy";
 import { GoogleStrategy } from "./strategies/google.strategy";
@@ -32,7 +31,6 @@ import { MailModule } from "../mail/mail.module";
   controllers: [AuthController],
   providers: [
     AuthService,
-    LocalStrategy,
     JwtStrategy,
     FtStrategy,
     GoogleStrategy,
